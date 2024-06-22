35aa352303d013a3e32fd6e05ff8de0df65036f4
Prashant Raj <prashant77399@gmail.com>
2024-06-23 03:20:01 +0530

    pushing lead data

---
+++ backend/init.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const Lead = require("./model/lead.js");
+
+main()
+    .then(() => {
+        console.log("connection successful");
+    })
+    .catch(err => console.log(err))
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/leading');
+}
+
+const lead1 = new Lead({
+    email: "xyz@gmail.com",
+    phoneNo: 7778885554,
+});
+
+lead1.save();
+
+Lead.insertMany([
+    {
+        email: "prashant77399@gmail.com",
+        phoneNo: 1225467893,
+    },
+    {
+        email: "abc.com",
+        phoneNo: 7755502211,
+    },
+    {
+        email: "ghi.com",
+        phoneNo: 7788888811,
+    },
+    {
+        email: "varshiththegreat857@gmail.com",
+        phoneNo: 1234567890,
+    },
+])
