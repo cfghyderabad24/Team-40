850604afc622e0990ee0ef00446e4b13bc6b8f59
Prashant Raj <prashant77399@gmail.com>
2024-06-23 00:00:50 +0530

    orders added

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const {v4 : uuidv4 } = require("uuid");
+const { faker } = require('@faker-js/faker');
+let orders = [];
+let getOrder = () => {
+    return {
+        orderId: uuidv4(),
+        trackingNo: faker.string.uuid(),
+        status: "Ordered",
+        orderDate: faker.date.past(),
+    };
+}
+
+for(let i = 0; i < 5; i += 1) {
+    orders.push(getOrder());
+}
