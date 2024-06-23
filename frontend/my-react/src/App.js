70a48dc6c77168c7a5b1cde168dd3a150294d1b8
gvarshithreddy <gvarshithreddy8@gmail.com>
2024-06-23 09:20:04 +0530

    merged commit

--- frontend/my-react/src/App.js
+++ frontend/my-react/src/App.js
@@ -1,40 +1,42 @@
-import React from 'react';
-import HomePage from './components/Homepage.js';
-import Chatbot from './components/Chatbot.js';
-import Products from './components/products/products.jsx';
-import Showcase from './components/statsdash.jsx';
-import About from './components/about_us/about_us/src/components/About.js';
-import { useState } from 'react';
-import Navbar from './components/Navbar';
+import React from "react";
+import HomePage from "./components/Homepage.js";
+
+import Products from "./components/products/products.jsx";
+import Showcase from "./components/statsdash.jsx";
+import About from "./components/about_us/about_us/src/components/About.js";
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import ChatBot from "./components/chatbot/ChatBot.jsx";
 const App = () => {
-  const [user, setUser] = useState("")
+  const [user, setUser] = useState("");
   
   return (
     <>
      <Navbar />
-    {(user == 'Parent' || user == 'Learner') &&<div>
-      <Products/>
-      <HomePage setUser = {setUser} user={user} />
-      <Chatbot />
-      
-    </div> }
-    {(user == '') && <div>
-      <HomePage setUser = {setUser} user={user} />
-      <Chatbot />
-      <Products/>
-      
-    </div> }
-    {(user == 'NGO'||user=='Govt Official') && <div>
-      <Showcase/>
-      <About/>
-      <HomePage setUser = {setUser} user={user} />
-      <Chatbot />
-      <Products/>
-      
-    </div> }
-    
+      <ChatBot />
+      {(user == "Parent" || user == "Learner") && (
+        <div>
+          <Products />
+          <HomePage setUser={setUser} user={user} />
+        </div>
+      )}
+      {user == "" && (
+        <div>
+          <HomePage setUser={setUser} user={user} />
+
+          <Products />
+        </div>
+      )}
+      {(user == "NGO" || user == "Govt Official") && (
+        <div>
+          <Showcase />
+          <About />
+          <HomePage setUser={setUser} user={user} />
+
+          <Products />
+        </div>
+      )}
     </>
-     
   );
 };
 
